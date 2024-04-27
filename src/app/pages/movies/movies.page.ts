dc84ec905d6bc243c79a90910d161fd7e6094355
deastwell <deanieleastwell@hotmail.com>
2024-04-27 19:33:26 +0200

    first commit

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  constructor(private movieService: MoviesService) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+ionic g page pages/movies
+ionic g page pages/movieDetails
+ionic g service aervices/movies
